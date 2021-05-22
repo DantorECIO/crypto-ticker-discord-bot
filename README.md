51d921aab42386ed83f9366fd73603a88c8c27a5
ManicRobot <manicrobot@protonmail.com>
2021-05-22 19:57:29 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# crypto-ticker-discord-bot
+
+A Discord bot that elegantly displays the price of any cryptocurrency in its nickname
+
+## About
+
+This Discord bot is more of a template to implement bots suited to any cryptocurrency [CoinGecko](https://www.coingecko.com/) supports. It's set to [Nano](https://nano.org/) by default.
+
+## Requirements
+
+- Python >= 3.8
+- discord.py >= 1.7.2
+- requests >= 2.23.0
+- python-dateutil >= 2.8.1
+- parsedatetime >= 2.6
+- pycoingecko >= 2.0.0
+
+The Python modules are listed in ``requirements.txt``, so you only need to do ``pip install -r requirements.txt``.
+
+## Configuration
+Go into ``config,json`` and change the following variables to suit your needs:
+
+- ``token``: Your Discord bot token.
+- ``cryptocurrency_id``: CoinGecko's ID of your cryptocurrency. You can look them up on their website, but it's usually something like ``nano``, ``btc``, ``eth`` etc.
+- ``cryptocurrency_name``: Can be anything you want, it's used in the name to neatly show the name of the cryptocurrency used.
+- ``fiat_id``: CoinGecko's ID of the fiat currency your cryptocurrency's price is compared against. You can look them up on their website, but it's usually something like ``eur``, ``usd``, ``gbp`` etc.
+- ``fiat_name``: Can be anything you want, it's used in the name to neatly show the name of the fiat currency used.
---
+++ cogs/ticker.py
@@ -0,0 +1,41 @@
+from discord.ext import commands
+from discord.ext import tasks
+import discord
+import pycoingecko
+
+
+class TickerCog(commands.Cog, name='Ticker'):
+    """Cog with the task to update the ticker"""
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.ticker_task.start()
+        
+    @tasks.loop(seconds=5)
+    async def ticker_task(self):
+        coingecko = pycoingecko.CoinGeckoAPI()
+        prices = coingecko.get_price(ids=self.bot.config["cryptocurrency_id"], vs_currencies='usd')
+        history = coingecko.get_coin_market_chart_by_id(id='nano', vs_currency=['usd'], days='1')
+        price_usd = round(prices[self.bot.config["cryptocurrency_id"]]['usd'], 4)
+        percentage = round(((prices[self.bot.config["cryptocurrency_id"]]['usd'] / history['prices'][0][1]) - 1) * 100, 2)
+        difference = round(prices[self.bot.config["cryptocurrency_id"]]['usd'] - history['prices'][0][1], 2)
+        c_name = self.bot.config["cryptocurrency_name"]
+        f_name = self.bot.config["fiat_name"]
+        for g in self.bot.guilds:
+            await g.me.edit(nick=f"{c_name}: {f_name}{price_usd:.2f}")
+        status = discord.Status.idle
+        if percentage < 2:
+            status = discord.Status.dnd
+        elif percentage > 2:
+            status = discord.Status.online
+        status_string = f"+{f_name}{difference:.2f} (+{percentage:.2f}%)" if percentage >= 0 else f"-{f_name}{abs(difference):.2f} ({percentage:.2f}%)"
+        await self.bot.change_presence(status=status, activity=discord.Activity(type=discord.ActivityType.watching, name=status_string))
+
+    @ticker_task.before_loop
+    async def before(self):
+        await self.bot.wait_until_ready()
+
+
+def setup(bot):
+    bot.add_cog(TickerCog(bot))
+
